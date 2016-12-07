20b791ac14e005302dc61cd396da8b46166257f4
Mkenini <mkeninismael2@gmail.com>
2016-12-08 00:09:04 +0100

    Edit de req_login.php

--- req_login.php
+++ req_login.php
@@ -9,7 +9,10 @@
 
     // ensuite on requête à nouveau la base pour l'utilisateur qui vient d'être inscrit, et
     $sql = $dbh->query("SELECT u.id, u.email, u.nom, u.prenom, u.couleur, u.profilepic FROM USERS u WHERE u.email='".$email."' AND u.password='".$password."'");
-    else {
+     if ($sql->rowCount()<1) {
+		 
+	 }
+	  else {
         // on récupère la ligne qui nous intéresse avec $sql->fetch(),
         // et on enregistre les données dans la session avec $_SESSION["..."]=...
         $result = $sql->fetch(PDO::FETCH_ASSOC);
