eef21386957f96587c4d212f7ccda83f9678e0e5
Mkenini <mkeninismael2@gmail.com>
2016-12-08 00:28:35 +0100

    Edit de quelques fichier .php

--- req_paint.php
+++ req_paint.php
@@ -4,5 +4,7 @@ if(!isset($_SESSION['prenom'])) {
     header("Location: main.php");  
 } 
 
+// Connect to server and select database.  
+$dbh = new PDO('mysql:host=localhost;dbname=pictionnary', 'test', 'test');
 
  ?>
